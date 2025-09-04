588baf9d978b0b0fb0b26c43edbde2f90f436b85
NOWBRO <wogud879@gmail.com>
2025-09-05 01:23:23 +0900

    feat: EmailAuth 라우트 추가 - signup, find-account 페이지 연결

--- react-login-app/src/App.js
+++ react-login-app/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import './App.css';
-import EmailLoginPage from './EmailLoginPage';
+import EmailLoginPage from './EmailAuth/EmailLoginPage';
+import SignupPage from './EmailAuth/SignupPage';
+import FindAccountPage from './EmailAuth/FindAccountPage';
 import SchedulePage from './SchedulePage';
 import WeatherPage from './WeatherPage';
 import JejuLocationPage from './JejuLocationPage';
@@ -20,6 +22,8 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/old" element={<HomePageOld />} />
                 <Route path="/email-login" element={<EmailLoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/find-account" element={<FindAccountPage />} />
                 <Route path="/schedule" element={<SchedulePage />} />
                 <Route path="/weather" element={<WeatherPage />} />
                 <Route path="/jeju-location" element={<JejuLocationPage />} />
